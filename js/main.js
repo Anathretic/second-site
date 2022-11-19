8343f119d838d8558105c30e05e9eed8dee254ab
Konrad <konrad.wojtylo.9@gmail.com>
2022-11-19 23:44:19 +0100

    added contact btn event

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ const nav = document.querySelector('.nav__box')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__box-item')
 const footerYear = document.querySelector('.footer__year')
+const userName = document.querySelector('#name')
+const userEmail = document.querySelector('#email')
+const userMsg = document.querySelector('#msg')
+const contactBtn = document.querySelector('.contact__form-btn')
+const contactHeading = document.querySelector('.contact h2')
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+const scrollBlockNavBtn = () => {
+    if(body.classList.contains('scroll-block')) {
+        body.classList.remove('scroll-block')
+    } else {
+        body.classList.add('scroll-block')
+    }
+}
 
 const handleNav = () => {
     nav.classList.toggle('nav__box--active')
@@ -18,28 +41,26 @@ const handleNav = () => {
     scrollBlockNavBtn()
 }
 
-const handleNavItemsAnimation = () => {
-    let delayTime = 0;
-
-    allNavItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's';
-        delayTime++;
-    })
-}
-
 const deleteAnimation = () => {
     allNavItems.forEach(item => {
         item.classList.remove('nav-items-animation')
     })
 }
 
-const scrollBlockNavBtn = () => {
-    if(body.classList.contains('scroll-block')) {
-        body.classList.remove('scroll-block')
+const msgBtnAction = () => {
+    if(userName.value !== '' && userEmail.value !== '' && userMsg.value !== '') {
+        contactHeading.textContent = 'sent!'
+        userName.value = ''
+        userEmail.value = ''
+        userMsg.value = ''
+
     } else {
-        body.classList.add('scroll-block')
+        contactHeading.textContent = 'Need more..'
     }
+
+    setTimeout(() => {
+        contactHeading.textContent = 'contact us!'
+    }, 3000)
 }
 
 const currentYear = () => {
@@ -48,4 +69,5 @@ const currentYear = () => {
 }
 
 currentYear()
+contactBtn.addEventListener('click', msgBtnAction)
 navBtn.addEventListener('click', handleNav)
