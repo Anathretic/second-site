a5de9dfff6950529061f276fb9721cf09d2a4f84
Konrad <konrad.wojtylo.9@gmail.com>
2022-12-30 22:50:23 +0100

    improved/changed desktop nav

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ const body = document.querySelector('body')
 const mobileNav = document.querySelector('.nav__mobile-box')
 const mobileNavBtn = document.querySelector('.burger-btn')
 const mobileAllNavItems = document.querySelectorAll('.nav__mobile-box-item')
-const desktopNav = document.querySelector('.nav__desktop-box')
-const desktopNavBtn = document.querySelector('.burger-btn-desktop')
-const desktopAllNavItems = document.querySelectorAll('.nav__desktop-box-item')
 const navIcon = document.querySelector('.nav__icon')
 
 const mobileBlock = () => {
@@ -15,28 +12,6 @@ const mobileBlock = () => {
 			body.classList.add('scroll-block')
 		}
 	}
-}
-
-const handleDesktopNavItemsAnimation = () => {
-	let delayTime = 0
-
-	desktopAllNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
-}
-
-const handleDesktopNav = () => {
-	desktopNav.classList.toggle('nav__desktop-box--active')
-
-	desktopAllNavItems.forEach(item => {
-		item.addEventListener('click', () => {
-			desktopNav.classList.remove('nav__desktop-box--active')
-		})
-	})
-
-	handleDesktopNavItemsAnimation()
 }
 
 const handleMobileNavItemsAnimation = () => {
@@ -70,15 +45,7 @@ const deleteMobileAnimation = () => {
 	})
 }
 
-const deleteDesktopAnimation = () => {
-	desktopAllNavItems.forEach(item => {
-		item.classList.remove('nav-items-animation')
-	})
-}
-
 navIcon.addEventListener('click', () => {
 	mobileNav.classList.remove('nav__mobile-box--active')
-	desktopNav.classList.remove('nav__desktop-box--active')
 })
 mobileNavBtn.addEventListener('click', handleMobileNav)
-desktopNavBtn.addEventListener('click', handleDesktopNav)
