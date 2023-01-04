cb8a28ba8d76b3892d9efa64ae1e8872060b6502
Konrad <konrad.wojtylo.9@gmail.com>
2023-01-04 22:01:19 +0100

    improved faq subpage

--- js/faq.js
+++ js/faq.js
@@ -1,9 +1,9 @@
 const faqInput = document.querySelector('.content__faq-input')
 const faqInputList = document.querySelector('.content__faq-list-menu')
-const allListElements = document.querySelectorAll('li')
+const faqInputListLi = document.querySelectorAll('.content__faq-list-menu li')
 
 const questionFilter = () => {
-    allListElements.forEach(el => {
+    faqInputListLi.forEach(el => {
         const match = new RegExp(faqInput.value, 'i').test(el.textContent)
         if(!match) {
             el.style.display = 'none'
@@ -16,6 +16,11 @@ const questionFilter = () => {
     if(faqInput.value === '') {
         faqInputList.style.display = 'none'
     }
+}
+
+const clearInput = () => {
+    faqInput.value = ''
+    faqInputList.style.display = 'none'
 }
 
 faqInput.addEventListener('keyup', questionFilter)
