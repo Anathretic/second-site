701c9699176e87918354ac80789c054ea98b8f82
Konrad <konrad.wojtylo.9@gmail.com>
2023-05-05 21:23:57 +0200

    changed some classes to clear the code

--- js/main.js
+++ js/main.js
@@ -14,35 +14,35 @@ const main = () => {
 
 const prepareDOMElements = () => {
 	BODY = document.querySelector('body')
-	MOBILE_NAV = document.querySelector('.nav__mobile-box')
-	DESKTOP_NAV = document.querySelector('.nav__desktop-box')
+	MOBILE_NAV = document.querySelector('.nav-mobile__box')
+	DESKTOP_NAV = document.querySelector('.nav-desktop__box')
 	MOBILE_NAV_BTN = document.querySelector('.burger-btn')
-	MOBILE_ALL_NAV_ITEMS = document.querySelectorAll('.nav__mobile-box-item')
-	DESKTOP_ALL_NAV_ITEMS = document.querySelectorAll('.nav__desktop-box-item')
+	MOBILE_ALL_NAV_ITEMS = document.querySelectorAll('.nav-mobile__item')
+	DESKTOP_ALL_NAV_ITEMS = document.querySelectorAll('.nav-desktop__item')
 	NAV_ICON = document.querySelector('.nav__icon')
 	DELAY_TIME = 1
 }
 
 const prepareDOMEvents = () => {
 	NAV_ICON.addEventListener('click', () => {
-		MOBILE_NAV.classList.remove('nav__mobile-box--active')
+		MOBILE_NAV.classList.remove('nav-mobile__box--active')
 	})
 	MOBILE_NAV_BTN.addEventListener('click', handleMobileNav)
 	handleDesktopNav()
 }
 
 const handleDesktopNav = () => {
-	DESKTOP_NAV.classList.add('nav__desktop-box--active')
+	DESKTOP_NAV.classList.add('nav-desktop__box--active')
 	handleDesktopNavItemsAnimation()
 }
 
 const handleMobileNav = () => {
-	MOBILE_NAV.classList.toggle('nav__mobile-box--active')
+	MOBILE_NAV.classList.toggle('nav-mobile__box--active')
 	BODY.classList.remove('scroll-block')
 
 	MOBILE_ALL_NAV_ITEMS.forEach(item => {
 		item.addEventListener('click', () => {
-			MOBILE_NAV.classList.remove('nav__mobile-box--active')
+			MOBILE_NAV.classList.remove('nav-mobile__box--active')
 			BODY.classList.remove('scroll-block')
 		})
 	})
@@ -52,7 +52,7 @@ const handleMobileNav = () => {
 }
 
 const mobileBlock = () => {
-	if (MOBILE_NAV.classList.contains('nav__mobile-box--active')) {
+	if (MOBILE_NAV.classList.contains('nav-mobile__box--active')) {
 		if (BODY.classList.contains('scroll-block')) {
 			BODY.classList.remove('scroll-block')
 		} else {
