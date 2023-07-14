ac0a757220e8c277a224685852453fa49f9ce903
Konrad <konrad.wojtylo.9@gmail.com>
2023-07-14 21:03:35 +0200

    Redesigned contact form, added nav to other subpages, changed content__special div

--- src/js/contactAndFooter.js
+++ src/js/contactAndFooter.js
@@ -3,6 +3,8 @@ let CONTACT_USERNAME
 let CONTACT_USER_EMAIL
 let CONTACT_USER_MSG
 let CONTACT_BTN
+let CONTACT_POPUP
+let CONTACT_POPUP_BTN
 let EMAIL_CHECK
 let INPUTS_ARR
 
@@ -17,7 +19,8 @@ const conFooPrepareDOMElements = () => {
 	CONTACT_USER_EMAIL = document.querySelector('#email')
 	CONTACT_USER_MSG = document.querySelector('#msg')
 	CONTACT_BTN = document.querySelector('.contact__form-btn')
-	EMAIL_CHECK = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i
+	CONTACT_POPUP = document.querySelector('.contact__popup')
+	CONTACT_POPUP_BTN = document.querySelector('.contact__popup-btn')
 	INPUTS_ARR = [CONTACT_USERNAME, CONTACT_USER_EMAIL, CONTACT_USER_MSG]
 }
 
@@ -25,23 +28,76 @@ const conFooPrepareDOMEvents = () => {
 	currentYear()
 	CONTACT_BTN.addEventListener('click', e => {
 		e.preventDefault()
-		msgBtnAction()
+
+		checkContactForm(INPUTS_ARR)
+		checkContactMail(CONTACT_USER_EMAIL)
+		checkContactErrors()
+	})
+	CONTACT_POPUP_BTN.addEventListener('click', closeContactPopup)
+}
+
+const showError = input => {
+	const formBox = input.parentElement
+	formBox.classList.add('warning')
+}
+
+const clearError = input => {
+	const formBox = input.parentElement
+	formBox.classList.remove('warning')
+}
+
+const clearAll = () => {
+	INPUTS_ARR.forEach(el => {
+		el.value = ''
+		el.classList.remove('warning')
 	})
 }
 
-const msgBtnAction = () => {
-	if (CONTACT_USERNAME.value !== '' && CONTACT_USER_MSG.value !== '' && EMAIL_CHECK.test(CONTACT_USER_EMAIL.value)) {
-		CONTACT_BTN.textContent = 'wysłane!'
-		INPUTS_ARR.forEach(el => {
-			el.value = ''
-		})
-	} else {
-		CONTACT_BTN.textContent = 'sprawdź formularz...'
-	}
+const checkContactForm = input => {
+	input.forEach(el => {
+		if (el.value === '' || el.value.length < 3) {
+			showError(el)
+		} else {
+			clearError(el)
+		}
+	})
+}
 
-	setTimeout(() => {
-		CONTACT_BTN.textContent = 'wyślij'
-	}, 2500)
+const checkContactMail = email => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email)
+	}
+}
+
+const checkContactErrors = () => {
+	const allInputs = document.querySelectorAll('.contact__form-box')
+	let errorCount = 0
+
+	console.log(allInputs);
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('warning')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		clearAll()
+		CONTACT_POPUP.classList.add('contact-popup-active')
+
+		setTimeout(() => {
+			CONTACT_POPUP.classList.remove('contact-popup-active')
+		}, 2000)
+	}
+}
+
+const closeContactPopup = () => {
+	CONTACT_POPUP.classList.remove('contact-popup-active')
 }
 
 const currentYear = () => {
